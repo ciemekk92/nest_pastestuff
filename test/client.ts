32379ed621692a7ec00d868e85cc3928f966e120
ciemekk92 <przemyslaw.reducha@gmail.com>
2023-03-08 08:49:23 +0100

    feat/integration tests

--- test/client.ts
+++ test/client.ts
@@ -1,4 +1,11 @@
 import { PrismaClient } from '@prisma/client';
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient({
+  datasources: {
+    db: {
+      url: 'postgresql://postgres:test@localhost:5434/postgres_test?schema=public',
+    },
+  },
+});
+
 export default prisma;
---
+++ test/snippets.e2e-spec.ts
@@ -0,0 +1,64 @@
+import { INestApplication } from '@nestjs/common';
+import { Test } from '@nestjs/testing';
+import * as request from 'supertest';
+import { SnippetsModule } from '../src/snippets/snippets.module';
+import prisma from './client';
+
+describe('Snippets', () => {
+  let app: INestApplication;
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [SnippetsModule],
+    }).compile();
+
+    app = moduleRef.createNestApplication();
+    await app.init();
+  });
+
+  afterEach(async () => {
+    const deleteSnippets = prisma.snippet.deleteMany();
+
+    await prisma.$transaction([deleteSnippets]);
+  });
+
+  afterAll(async () => {
+    await prisma.$disconnect();
+    await app.close();
+  });
+
+  it('/POST should create new snippet', async () => {
+    const test = await request(app.getHttpServer())
+      .post('/snippets')
+      .send({ content: 'Test snippet post' })
+      .set('Content-Type', 'application/json')
+      .expect(201);
+
+    expect(test.body).toHaveProperty('content', 'Test snippet post');
+  });
+
+  it('/GET should return all snippets', async () => {
+    await prisma.snippet.createMany({
+      data: [{ content: 'Test snippet 1' }, { content: 'Test snippet 2' }],
+    });
+
+    const test = await request(app.getHttpServer())
+      .get('/snippets')
+      .expect(200);
+
+    expect(test.body).toHaveLength(2);
+  });
+
+  it('/GET:id should return snippet with given id', async () => {
+    const createdSnippet = await request(app.getHttpServer())
+      .post('/snippets')
+      .send({ content: 'Test snippet' })
+      .set('Content-Type', 'application/json')
+      .expect(201);
+
+    await request(app.getHttpServer())
+      .get(`/snippets/${createdSnippet.body.id}`)
+      .expect(200)
+      .expect(createdSnippet.body);
+  });
+});
