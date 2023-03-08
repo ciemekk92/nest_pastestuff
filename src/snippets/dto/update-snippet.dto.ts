fbdd58d95e072efa56de7315a0bd20ca1933b188
ciemekk92 <przemyslaw.reducha@gmail.com>
2023-03-08 10:39:06 +0100

    feat/add swagger to API

--- src/snippets/dto/update-snippet.dto.ts
+++ src/snippets/dto/update-snippet.dto.ts
@@ -1,6 +1,8 @@
 import { PartialType } from '@nestjs/mapped-types';
+import { ApiProperty } from '@nestjs/swagger';
 import { CreateSnippetDto } from './create-snippet.dto';
 
 export class UpdateSnippetDto extends PartialType(CreateSnippetDto) {
+  @ApiProperty()
   content?: string;
 }
